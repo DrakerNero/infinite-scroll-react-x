553b86be46b3a1c97aa354a9664bc23aa1f01c40
Pongnarin Tawan <pongnarin.thawan@gmail.com>
2017-03-03 17:54:50 +0700

    test(Test): New test file

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,47 @@
-const expect = require('chai').expect;
+const expect = require('chai').expect
+const isEmpty = require('../src/helper').isEmpty
 
-describe('BetterImg', function() {
-  describe('all', function() {
-    it('should be an array of value|label', function(){
+describe('Infinite-scroll-react-x', () => {
+  describe('all', () => {
+    it('should be an array of value|label', () => {
       expect(true).to.equal(true);
     });
   });
-});
+
+  describe('isEmpty', () => {
+    it('should be object = {} return true', () => {
+      expect(isEmpty({})).to.equal(true)
+    })
+
+    it('should be object = {x1: 1, x2: 2}', () => {
+      expect(isEmpty({
+        x1: 1,
+        x2: 2,
+      })).to.equal(false)
+    })
+
+    it('should be array = [] return true', () => {
+      expect(isEmpty([])).to.equal(true)
+    })
+
+    it('should be array = [1, 2, 3] return false', () => {
+      expect(isEmpty([1, 2, 3])).to.equal(false)
+    })
+
+    it('should be "" = return true', () => {
+      expect(isEmpty('')).to.equal(true)
+    })
+
+    it('should be "hello" = return false', () => {
+      expect(isEmpty('')).to.equal(true)
+    })
+
+    it('should be null = return ture', () => {
+      expect(isEmpty(null)).to.equal(true)
+    })
+
+    it('should be undefined = return ture', () => {
+      expect(isEmpty(undefined)).to.equal(true)
+    })
+  })
+})
